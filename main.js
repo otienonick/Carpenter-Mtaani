6379f7b67c7d3d1029500d229e1938ae39aa40f7
otienonick <nick.odongi@student.moringaschool.com>
2021-08-04 12:00:49 +0300

    added scroll button duration

--- main.js
+++ main.js
@@ -4,14 +4,62 @@ function validateForm(){
     let myNumber = document.getElementById('phoneNumber').value;
     let myLocation = document.getElementById('location').value;
     let myFile = document.getElementById('file').value;
-    if(myName !='' && myNumber!='' && myLocation!='' && myFile !=''){
-        $('#message').slideToggle(1500);
-        $('form#myForm').reset();
-      
 
-       
+        if(myName==''){
+            $('#error1').show(800);
+        }
+            else{
+                $('#error1').hide(300);
 
+            }
+        
+         if(myNumber===''){
+            $('#error2').show(800);
+        }else{
+
+            $('#error2').hide(300);
+        }
+             if(myLocation ===''){
+                $('#error3').show(800);
+
+            } else {
+                $('#error3').hide(300);
+
+            }
+           if (myFile ===''){
+                return false;
+            }
+            if(myName!=''&& myNumber!=''&& myLocation !='' && myFile!=''){
+            
+                $('#message').slideToggle(1500);
+                $('#name').val('');
+                $('#phoneNumber').val('');
+                $('#location').val('');
+                $('#file').val('');
+            }
+ 
     };
 
+// scroll up button
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 800) {
+            $('.up-btn').fadeIn();
+        } else {
+            $('.up-btn').fadeOut();
+        }
+    });
+
+    $('.up-btn').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 50);
+        return false;
+    });
+
+});
+
+
     
-};
+
